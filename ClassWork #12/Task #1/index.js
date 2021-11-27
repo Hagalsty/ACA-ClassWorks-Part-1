aa2e2c5a05fd9732d9eb1aab2bb3a2b18b554dab
Hayk Galstyan <hayk.galstyan99@gmail.com>
2021-11-27 15:55:59 +0400

    feat(Task #1): add helpers, assets, constants

--- ClassWork #12/Task #1/index.js
+++ ClassWork #12/Task #1/index.js
@@ -1,27 +1,9 @@
-const buttonElement = document.getElementById("b1");
-const pElement = document.getElementById("p1");
+import { getDate } from "./helpers/getDate.js";
 
-buttonElement.addEventListener("click", function () {
-  if (pElement.innerText == "") {
-    const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    const year = new Date().getFullYear();
-    const day = new Date().getDate();
-    const month = months[new Date().getMonth()];
-    pElement.innerText = `${day}/${month}/${year}`;
-  } else {
-    pElement.innerText = "";
-  }
+const buttonForDate = document.getElementById("buttonForDate");
+const dateWrapper = document.querySelector(".placeForNewDate");
+
+buttonForDate.addEventListener("click", function () {
+  dateWrapper.classList.toggle("placeForNewDate");
+  dateWrapper.innerText = getDate();
 });

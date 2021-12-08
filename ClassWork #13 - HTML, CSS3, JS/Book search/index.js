4191b7a41ada71cd4e776e6adf6da0789d2d2c93
Hayk Galstyan <hayk.galstyan99@gmail.com>
2021-12-08 19:00:10 +0400

    Change Folder Name

---
+++ ClassWork #13 - HTML, CSS3, JS/Book search/index.js
@@ -0,0 +1,43 @@
+const searchBtn = document.getElementById("searchBtn");
+const bookName = document.getElementById("bookName");
+const tbody = document.getElementsByTagName("tbody");
+
+async function getData(name) {
+  const res = await fetch("http://openlibrary.org/search.json?q=" + name)
+    .then((r) => r.json())
+    .then((r) => r);
+  return res;
+}
+
+function createList(arr) {
+  arr.forEach(
+    ({
+      title,
+      author_name: author = "Unknown",
+      first_publish_year: publised = "Unknown",
+      subject = "Unknown",
+    }) => {
+      const subjects = subject === "Unknown" ? "Unknown" : subject.slice(0, 5);
+
+      const row = document.createElement("tr");
+      const titleTd = document.createElement("td");
+      titleTd.innerText = title;
+      const authorTd = document.createElement("td");
+      authorTd.innerText = author;
+      const publishedTd = document.createElement("td");
+      publishedTd.innerText = publised;
+      const subjectTd = document.createElement("td");
+      subjectTd.innerText = subjects;
+      row.append(titleTd, authorTd, publishedTd, subjectTd);
+      tbody[0].append(row);
+    }
+  );
+}
+
+searchBtn.addEventListener("click", async function () {
+  const name = bookName.value.replaceAll(" ", "+");
+  const res = await getData(name);
+//   const pages = 
+  createList(res.docs);
+  console.log(res);
+});

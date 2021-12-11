f1851222323ac428b820c62063481e0d799bc571
naira <yeghiazaryannaira97@gmail.com>
2021-12-11 20:07:31 +0400

    refactor(Films): some improvements

--- ClassWork #13 - HTML, CSS3, JS/Films/getData.js
+++
@@ -1,8 +0,0 @@
-const url = "https://ghibliapi.herokuapp.com/films/";
-
-export async function getData() {
-  const res = await fetch(url)
-    .then((r) => r.json())
-    .then((r) => r);
-  return res;
-}

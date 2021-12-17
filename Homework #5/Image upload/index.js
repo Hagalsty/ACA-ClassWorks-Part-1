d57e452d57cdce85c1572a73e6b67518acfd8f5a
Hayk Galstyan <hayk.galstyan99@gmail.com>
2021-12-17 16:35:22 +0400

    separeting into parts for Part 1

--- Homework #5/Image upload/index.js
+++
@@ -1,7 +0,0 @@
-const fileInp = document.getElementById("fileInp");
-const img = document.querySelector("img");
-const uploadBtn = document.getElementById("uploadBtn");
-
-uploadBtn.addEventListener("click", function (event) {
-  img.src = URL.createObjectURL(fileInp.files[0]);
-});
